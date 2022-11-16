4607f4b53160445880e5fd1c806f48c8632de312
dgponce <dg38828@gmail.com>
2022-11-16 11:31:30 -0600

    Added cart context and reducer

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,24 @@
+import { useContext } from 'react';
+
+import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 
-const HeaderCartButton = props => {
-  return <button className={styles.button} onClick={props.onClick}>
-    <span className={styles.icon}>
-      <CartIcon />
-    </span>
-    <span>Your Cart</span>
-    <span className={styles.badge}>
-      3
-    </span>
-  </button>
+const HeaderCartButton = (props) => {
+  const ctx = useContext(CartContext);
+  const cartItems = ctx.items.reduce((currNumber, item) => {
+    return currNumber + item.amount;
+  }, 0);
+
+  return (
+    <button className={styles.button} onClick={props.onClick}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={styles.badge}>{cartItems}</span>
+    </button>
+  );
 };
 
 export default HeaderCartButton;
