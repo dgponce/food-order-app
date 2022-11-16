eef285e65a5f9205f0f3fa0ee24acfbd1be7aaf6
dgponce <dg38828@gmail.com>
2022-11-16 13:19:40 -0600

    Added cart button style using side effect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,37 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [ btnIsHighlighted, setBtnIsHighlighted ] = useState(false);
   const ctx = useContext(CartContext);
-  const cartItems = ctx.items.reduce((currNumber, item) => {
+  const { items } = ctx;
+
+  const cartItems = items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    },300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items])
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
