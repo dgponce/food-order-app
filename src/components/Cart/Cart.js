6c910bd38c76021f800a672f9104dfab47431f47
dgponce <dg38828@gmail.com>
2022-11-16 13:03:08 -0600

    Add remove items logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,24 +11,25 @@ const Cart = (props) => {
   const totalAmount = `$${ctx.totalAmount.toFixed(2)}`;
   const hasItems = ctx.items.length > 0;
 
-  const cartItemRemoveHandler = id => {
-
+  const cartItemRemoveHandler = (id) => {
+    ctx.removeItem(id);
   };
 
-  const cartItemAddHandler = item => {
-
+  const cartItemAddHandler = (item) => {
+    const cartItem = { ...item, amount: 1 };
+    ctx.addItem(cartItem);
   };
 
   const cartItems = (
     <ul className={styles['cart-items']}>
       {ctx.items.map((item) => (
-        <CartItem 
+        <CartItem
           key={item.id}
           name={item.name}
           amount={item.amount}
           price={item.price}
           onRemove={cartItemRemoveHandler.bind(null, item.id)}
-          onAdd={cartItemAddHandler(null, item)}
+          onAdd={cartItemAddHandler.bind(null, item)}
         />
       ))}
     </ul>
@@ -42,7 +43,9 @@ const Cart = (props) => {
         <span>{totalAmount}</span>
       </div>
       <div className={styles.actions}>
-        <button className={styles['button--alt']} onClick={props.onHideCart}>Close</button>
+        <button className={styles['button--alt']} onClick={props.onHideCart}>
+          Close
+        </button>
         {hasItems && <button className={styles.button}>Order</button>}
       </div>
     </Modal>
